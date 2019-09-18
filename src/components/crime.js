1c1124515ef7c49d39729b044c2a506f1a800d82
Christopher Gonzales <cm.gonzales@yahoo.com>
2019-09-17 19:56:07 -0700

    fixed map function bug

--- src/components/crime.js
+++ src/components/crime.js
@@ -1,76 +1,63 @@
+import React from "react";
+import axios from "axios";
+import "../style/crime.css";
 
-import React from 'react';
-import axios from 'axios';
-import '../style/crime.css'
+class crime extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { crime: [], value: "" };
+  }
 
+  handleChange = e => {
+    this.setState({ value: e.target.value });
+  };
 
-class crime extends React.Component{
+  results = (e) => {
+    const self = this;
 
-constructor(props){
-  super(props)
- this.state = {crime: [],
- value: ""
-};
-
-}
-
- handleChange = (e) =>{
-    
-       this.setState({value: e.target.value});
-        
-            
-         }
-      
-
-clickMe=()=>{
-    
-  
-  const self = this;
-
-  
-  var url = `https://data.sfgov.org/resource/cuks-n6tp.json?pddistrict=MISSION&dayofweek=Friday&$order=date%20DESC&$where=date%3E%272018-01-01T12:00:00%27&category=${this.state.value}`
-  axios.get(url).then(function(response){
-    self.setState({crime: response.data});
-  })
+    var url = `https://data.sfgov.org/resource/cuks-n6tp.json?pddistrict=MISSION&dayofweek=Friday&$order=date%20DESC&$where=date%3E%272018-01-01T12:00:00%27&category=${this.state.value}`;
+    axios.get(url).then(function(response) {
+      self.setState({ crime: response.data });
+    });
 
 
-}
 
-render(){
-  return(
-      <div className = 'crime'>
-    <select className ='select'
-    value={this.state.selectValue} 
-    onChange={this.handleChange} 
-   > <option value="Select">PLEASE SELECT</option>
-    <option value="LARCENY/THEFT">LARCENY/THEFT</option>
-    <option value="ROBBERY">ROBBERY</option>
-    <option value="ASSAULT">ASSAULT</option>
-    <option value="DRUG/NARCOTIC">DRUG/NARCOTIC</option>
-  </select>
-  <button
-    className="btn btn-primary"
-    value = {this.state.value}
-    onClick={this.clickMe}>
-    submit 
-  </button>
-  <h3 className = 'nameInsert'>{this.state.value}</h3>    
-        {this.state.crime.map(function(stat, i){
-          return(
-              <ul className = 'records'>
-              <li key={i}>{stat.address} {stat.time} {stat.descript} </li>
-              </ul>
+  };
 
-          )
-        })}
-    </div>
-    
-  )
-
-
-}
-
-
+  render() {
+    return (
+      <div className="crime">
+        <select
+          className="select"
+          value={this.state.selectValue}
+          onChange={this.handleChange}
+        >
+          {" "}
+          <option value="Select">PLEASE SELECT</option>
+          <option value="LARCENY/THEFT">LARCENY/THEFT</option>
+          <option value="ROBBERY">ROBBERY</option>
+          <option value="ASSAULT">ASSAULT</option>
+          <option value="DRUG/NARCOTIC">DRUG/NARCOTIC</option>
+        </select>
+        <button
+          className="btn btn-primary"
+          value={this.state.value}
+          onClick={this.results}
+        >
+          submit
+        </button>
+        <h3 className="nameInsert">{this.state.value}</h3>
+        {this.state.crime.slice(0,20).map(stat  => 
+        <ul className="records">
+              <li>
+                {stat.address} {stat.time} {stat.descript}{" "}
+              </li>
+            </ul>
+          
+        )}
+      </div>
+    );
+  }
 }
 
 export default crime;
